33f59d921867dc1695fe71a7d16640adafe6c52a
Dylan Lee <dylan@aidkr.com>
2022-06-10 11:13:52 +0900

    answer

--- lib/my_redis.ex
+++ lib/my_redis.ex
@@ -5,7 +5,8 @@ defmodule MyRedis do
   # mix test 를 통과하면 구현이 완료된 것입니다.
 
   def start_link() do
-    GenServer.start_link(MyRedis, Map.new())
+    # GenServer.start_link(MyRedis, Map.new())
+    GenServer.start_link(__MODULE__, Map.new())
   end
 
   def get(pid, key) do
@@ -13,30 +14,26 @@ defmodule MyRedis do
   end
 
   def set(pid, key, value) do
-    GenServer.call(pid, {:set, key, value})
+    GenServer.cast(pid, {:set, key, value})
   end
 
   def delete(pid, key) do
-    GenServer.call(pid, {:delete, key})
+    GenServer.cast(pid, {:delete, key})
   end
 
   def init(init_mem) do
       {:ok, init_mem}
   end
 
-  def handle_call({:get, key} , _from, mem) do
-      if Map.has_key?(mem, key) do
-          {:reply, {:ok, Map.get(mem, key)}, mem}
-      else
-          {:reply, :error, mem}
-      end
+  def handle_call({:get, key} , _from, mem ) do
+      { :reply, mem |> Map.fetch(key), mem }
   end
 
-  def handle_call({:set, key, value}, _from, mem ) do
-      {:reply, :ok, Map.put(mem, key, value)}
+  def handle_cast({:set, key, value}, mem ) do
+      {:noreply, mem |> Map.put(key, value) }
   end
 
-  def handle_call({:delete, key}, _from, mem) do
-      {:reply, :ok, Map.delete(mem, key)}
+  def handle_cast({:delete, key}, mem) do
+      {:noreply, mem |> Map.delete(key)}
   end
 end
